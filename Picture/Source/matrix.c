6cf265332345d259bdcae26e9db7091067dc1003
Aurélien Veillard <aurelien.veillard@univ-tlse3.fr>
2013-12-12 21:55:42 +0100

    Add picture part.
    Signed-off-by: Aurélien Veillard <aurelien.veillard@univ-tlse3.fr>

---
+++ Picture/Source/matrix.c
@@ -0,0 +1,70 @@
+//======================================================================//
+// Projet Informatique: Indexation de Fichiers
+//======================================================================//
+// UPSSITECH L3 SMI 2013-2014
+// Auteur: VEILLARD Aurélien
+// Date de création: 30/11/13
+//======================================================================//
+// Fichier C: matrix.c
+//======================================================================//
+// Fonctions:
+// - Lecture du contenu des fichiers images (au format .txt)
+// - Stockage des matrices des trois composantes par alloc dynamique
+// - Libération de la mémoire allouée
+//======================================================================//
+// REPRESENTATION MATRICE 2D:
+//
+//	  | [] [] [] [] [] [] [] [] [] []
+//	  | [] [] [] [] [] [] [] [] [] []
+//	  | [] [] [] [] [] [] [] [] [] []
+// 	  | [] [] [] [] [] [] [] [] [] []
+//  sizeY | [] [] [] [] [] [] [] [] [] []
+// nbLigne| [] [] [] [] [] [] [] [] [] []
+//	  | [] [] [] [] [] [] [] [] [] []
+//	  | [] [] [] [] [] [] [] [] [] []
+//	  | [] [] [] [] [] [] [] [] [] []
+//	  | [] [] [] [] [] [] [] [] [] []
+//	    _____________________________
+//		  sizeX = nbColonne
+//
+//======================================================================//
+
+#include "matrix.h"
+
+//======================================================================//
+// Allocation dynamique de matrice x*y
+
+int** allocMatrix2D(int y, int x, FILE* log) {
+	int i = 0;
+	int** matrix = NULL;	// Initialisation d'une matrice dynamique
+	matrix = (int**)malloc(y*sizeof(int*));	// Malloc de la matrice (1er tableau)
+	
+	// Parcours du premier tableau (le tableau de pointeurs vers les tableaux qui formeront la matrice)
+	for(i = 0; i < y; i++) {
+		// Malloc des tableaux de la matrice
+		matrix[i] = (int*)malloc(x*sizeof(int));
+	}
+	
+	// Test pour savoir si la matrice a bien été crée
+	if(matrix == NULL) {
+		fprintf(log, "> ERROR: Matrix creation failed\n");
+		fflush(stdout);
+	}
+	return matrix;
+}
+
+//======================================================================//
+
+int freeMatrix2D(int** matrix2D, int y, FILE* log) {
+	int i;
+	
+	// Parcours du tableau de pointeurs vers les tableaux
+	for(i = 0; i < y; i++) {
+		// Free des tableaux de la matrice
+		free(matrix2D[i]);
+	}
+	
+	// Free du tableau à l'origine de la matrice
+	free(matrix2D);
+}
+//======================================================================//
